5529b26d2c4d7ac0ea59243ddc55c9ca424fc4a7
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-19 12:05:11 +0100

    Mapped entity to collection

--- Citizen/src/main/java/com/project/entity/Citizen.java
+++ Citizen/src/main/java/com/project/entity/Citizen.java
@@ -1,7 +1,9 @@
 package com.project.entity;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "citizen_subset")
 public class Citizen {
 
 	@Id
