cb7ae4c75036a41809b497d18670ea1422eaf062
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-19 13:11:57 +0100

    Added basic repo tests

--- Citizen/src/test/java/com/project/repository/CitizenRepoTest.java
+++ Citizen/src/test/java/com/project/repository/CitizenRepoTest.java
@@ -2,33 +2,52 @@ package com.project.repository;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.project.controller.CitizenController;
 import com.project.entity.Citizen;
-import com.project.service.CitizenService;
+import com.project.service.CitizenServiceImpl;
 
 @RunWith(SpringRunner.class)
-public class RepoTest {
+@SpringBootTest
+public class CitizenRepoTest {
 
-	@Autowired
-	private CitizenController controller;
+	@InjectMocks
+	private CitizenServiceImpl service;
 
-	@Autowired
-	private CitizenService service;
+	@Mock
+	private CitizenRepo repo;
 
-	private static final Citizen citizen1 = new Citizen(12345679L, "Tan", "Dryden", "Ilford, London", "18/07/1994",
-			"Essex", "female");
-	
-	
+	private static final Citizen TEST_CITIZEN_1 = new Citizen(12345679L, "Tan", "Dryden", "Ilford, London",
+			"18/07/1994", "Essex", "female");
+	private static final Citizen TEST_CITIZEN_2 = new Citizen(12345678L, "Harry", "Buck", "Romford, London",
+			"02/10/1995", "Essex", "male");
+
+	private List<Citizen> citizenList;
+
+	@Before
+	public void setup() {
+		citizenList = new ArrayList<>();
+		citizenList.add(TEST_CITIZEN_1);
+		citizenList.add(TEST_CITIZEN_2);
+	}
+
 	@Test
-	public void getCitizenTest(){
-		service.getCitizen();
-		assertEquals(citizen1.toString(), service.getCitizen().toString());
-		
+	public void getCitizenTest() {
+		Mockito.when(repo.findAll()).thenReturn(citizenList);
+		List<Citizen> returnedCitizens = service.getCitizen();
+		assertEquals(citizenList, returnedCitizens);
+		assertEquals(true, returnedCitizens.contains(TEST_CITIZEN_2));
+
 	}
 
 }
