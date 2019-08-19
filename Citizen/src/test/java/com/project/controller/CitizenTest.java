649930e6b8a89f1c9cd934490b6c8fa33cca4a09
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-19 13:11:48 +0100

    Fixed tests for CitizenTest

--- Citizen/src/test/java/com/project/controller/CitizenTest.java
+++ Citizen/src/test/java/com/project/controller/CitizenTest.java
@@ -12,40 +12,37 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-
 import org.springframework.web.client.RestTemplate;
 
 import com.project.entity.Citizen;
 import com.project.service.CitizenService;
 
-
-
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CitizenTest{
+public class CitizenTest {
 
 	@InjectMocks
-	CitizenController controller;
+	private CitizenController controller;
 
 	@Mock
-	CitizenService service;
+	private CitizenService service;
 
 	@Mock
-	RestTemplate restTemplate;
+	private RestTemplate restTemplate;
 
-    private static final Citizen citizen1= new Citizen(12345679L, "Tan", "Dryden", "Ilford, London", "18/07/1994", "Essex", "female");
-	private static final Citizen citizen2= new Citizen(12345779L, "Chris", "Holmes", "Barking, London", "01/07/1974", "Essex", "male");
-	
+	private static final Citizen citizen1 = new Citizen(12345679L, "Tan", "Dryden", "Ilford, London", "18/07/1994",
+			"Essex", "female");
+	private static final Citizen citizen2 = new Citizen(12345779L, "Chris", "Holmes", "Barking, London", "01/07/1974",
+			"Essex", "male");
+
 	@Test
 	public void getCitizenTest() {
-	    List<Citizen> Mock_List =new ArrayList<>();
+		List<Citizen> Mock_List = new ArrayList<>();
 		Mock_List.add(citizen1);
 		Mock_List.add(citizen2);
 		Mockito.when(service.getCitizen()).thenReturn(Mock_List);
 		assertEquals(Mock_List, controller.getCitizen());
 		Mockito.verify(service).getCitizen();
 	}
-	
+
 }
