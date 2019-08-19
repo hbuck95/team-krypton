ec000fdc5f1dad19b281e143f372d7b8cb1b6237
Jack Flanagan <jack.flanagan93@outlook.com>
2019-08-19 16:55:25 +0100

    autowired

--- ANPR/src/main/java/com/project/controller/ANPRController.java
+++ ANPR/src/main/java/com/project/controller/ANPRController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import com.project.entity.ANPRCameraEntity;
 import com.project.entity.ANPRObservationEntity;
@@ -21,25 +20,18 @@ import com.project.service.VehicleRegistrationService;
 @RequestMapping("/ANPR")
 public class ANPRController {
 
-	private RestTemplate restTemplate;
 	private ANPRCameraService anprCameraService;
 	private ANPRObservationService anprObservationService;
 	private VehicleRegistrationService vehicleRegistrationService;
 
-	private String forenames;
-	private String surname;
 	private String vehicleRegistrationNo;
-
-	private VehicleRegistrationService vrs;
-	private ANPRCameraService acs;
-	private ANPRObservationService aos;
 
 	@Autowired
 	public ANPRController(ANPRCameraService anprCameraService, ANPRObservationService anprObservationService,
 			VehicleRegistrationService vehicleRegistrationService) {
-		this.vehicleRegistrationService = vrs;
-		this.anprCameraService = acs;
-		this.anprObservationService = aos;
+		this.vehicleRegistrationService = vehicleRegistrationService;
+		this.anprCameraService = anprCameraService;
+		this.anprObservationService = anprObservationService;
 	}
 
 	// get vehicleRegistrations(forenames, surname)
@@ -48,26 +40,26 @@ public class ANPRController {
 
 	@GetMapping("/getAll")
 	public List<VehicleRegistrationEntity> getAll() {
-		return vrs.getAll();
+		return vehicleRegistrationService.getAll();
 	}
 
 	@PostMapping("/getVehicleRegistrations")
 	public List<VehicleRegistrationEntity> getVehicleRegistrations(
 			@RequestBody VehicleRegistrationEntity vehicleRegistrationEntity) {
 
-		return vrs.findByForenamesAndSurname(vehicleRegistrationEntity);
+		return vehicleRegistrationService.findByForenamesAndSurname(vehicleRegistrationEntity);
 	}
 
 	@PostMapping("/getANPRObservations")
 	public List<ANPRObservationEntity> getANPRObservations(@RequestBody String vehicleRegistration) {
 
-		return aos.findByVehicleRegistrationNo(vehicleRegistrationNo);
+		return anprObservationService.findByVehicleRegistrationNo(vehicleRegistrationNo);
 	}
 
 	@PostMapping("/getANPRCamera")
 	public List<ANPRCameraEntity> getANPRCamera(@RequestBody String id) {
 
-		return acs.findById(id);
+		return anprCameraService.findById(id);
 	}
 
 }
