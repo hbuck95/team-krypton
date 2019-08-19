d9df7a24bb25f64526c9bcabb31a153f4e529960
Jack Flanagan <jack.flanagan93@outlook.com>
2019-08-19 12:44:05 +0100

    creating controller endpoints

--- ANPR/src/main/java/com/project/controller/ANPRController.java
+++ ANPR/src/main/java/com/project/controller/ANPRController.java
@@ -1,5 +1,7 @@
 package com.project.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -18,5 +20,28 @@ public class ANPRController {
 	private ANPRObservationService anprObservationService;
 	private VehicleService vehicleService;
 	private VehicleRegistrationService vehicleRegistrationService;
+
+	// get vehicleRegistrations(forenames, surname)
+	// get ANPRObservations(vehicleRegistration)
+	// get ANPRCamera(id, timestamp)
+
+	@PostMapping("/getVehicleRegistrations")
+	public String getVehicleRegistrations(@RequestBody String forenames, @RequestBody String surname) {
+
+		return "g";
+	}
+
+	@PostMapping("/getANPRObservations")
+	public String getANPRObservations(@RequestBody String vehicleRegistration) {
+
+		return "g";
+
+	}
+
+	@PostMapping("/getANPRCamera")
+	public String getANPRCamera(@RequestBody String id) {
+
+		return "g";
+	}
 
 }
