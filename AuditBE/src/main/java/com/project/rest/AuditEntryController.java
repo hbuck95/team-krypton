9f35acc006b513c0748002af2d2b4f4c5b72ab5e
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-20 15:37:52 +0100

    Created AuditEntryController with Endpoints

---
+++ AuditBE/src/main/java/com/project/rest/AuditEntryController.java
@@ -0,0 +1,41 @@
+package com.project.rest;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.entities.AuditEntry;
+import com.project.service.AuditService;
+
+@RestController
+@RequestMapping("/audits")
+public class AuditEntryController {
+
+	private AuditService service;
+
+	@Autowired
+	public AuditEntryController(AuditService service) {
+		this.service = service;
+	}
+
+	@GetMapping("/getAll")
+	public Collection<AuditEntry> getAllAudits() {
+		return service.getAllAudits();
+	}
+
+	@PostMapping("/getForUser")
+	public Collection<AuditEntry> getAuditsForUser(@RequestBody AuditEntry entry) {
+		return service.getAuditsForUser(entry.getUsername());
+	}
+
+	@PostMapping("/create")
+	public String createAudit(@RequestBody AuditEntry entry) {
+		return service.createAudit(entry);
+	}
+
+}
