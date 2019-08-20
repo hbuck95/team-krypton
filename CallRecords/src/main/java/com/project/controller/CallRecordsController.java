66c819de464ff6f9753a806bc7c69fd83a0bd4ac
Tania <tdrydendiaz@gmail.com>
2019-08-20 14:50:33 +0100

    fin pre test

--- CallRecords/src/main/java/com/project/controller/CallRecordsController.java
+++ CallRecords/src/main/java/com/project/controller/CallRecordsController.java
@@ -35,10 +35,36 @@ public class CallRecordsController {
 //	get CellTowers(Lat,long,time,range)
 //	return List<CellTowerEntities>
 	
-	
+
+//	GET SubscriberRecords(forenames, surname)
+//	return SubscriberRecords : SubscriberRecordsEntity
+
+	@PostMapping("/subscriberEntityByForenameSurname")
+	public List<SubscriberRecordsEntity> getSubscriberEntitybyForenameSurname(
+			@RequestBody SubscriberRecordsEntity subscriberRecordEntity) {
+		return srService.findByForenameSurname(subscriberRecordEntity);
+
+	}
+
+//	GET MobileCallRecords(Caller MSISDN)
+//	return Matching MSISDN's : List <CallRecordEntity>
+
+	@PostMapping("/mobileCallRecordsMSISDN")
+	public List<MobileCallRecordsEntity> getMobileCallRecordByMSISDN(
+			@RequestBody MobileCallRecordsEntity mobileCallRecordsEntity) {
+		return mcrService.findByMSISDN(mobileCallRecordsEntity);
+	}
 
 	
-	
+
+//	Get CellTowerEntity(towerId)
+//	return List<CellTowerEntity>
+
+	@PostMapping("/cellTowerById")
+	public List<CellTowersEntity> getCellTowersEntityById(@RequestBody CellTowersEntity cellTowerEntity) {
+		return ctService.findById(cellTowerEntity);
+	}
+
 //	get MobileCallRecords(cellTowerId, time)
 //	return List<CallRecordsEntities>
 
@@ -48,9 +74,6 @@ public class CallRecordsController {
 		return mcrService.findById(mobileCallRecordsEntity);
 	}
 
-	
-	
-	
 //	get SubscriberRecords(callerMSISDN)
 //	return List<SusbscriberRecordEntity>
 
@@ -62,67 +85,24 @@ public class CallRecordsController {
 
 	}
 
-
-
-//	GET MobileCallRecords(Caller MSISDN)
-//	return Matching MSISDN's : List <CallRecordEntity>
-	
-	@PostMapping("/mobileCallRecordsMSISDN")
-	public List<MobileCallRecordsEntity> getMobileCallRecordByMSISDN(@RequestBody MobileCallRecordsEntity mobileCallRecordsEntity){
-		return mcrService.findByMSISDN(mobileCallRecordsEntity);
-	}
-	
-	
-	
 //			get CallRecords(receiverMSISDN)
 //			return List<callRecordEntity>
-	
+
 	@PostMapping("/callRecordsByReceiverMSISDN")
-	public List<MobileCallRecordsEntity> getCallRecordByReceiverMSISDN(@RequestBody MobileCallRecordsEntity mobileCallRecordsEntity){
-		return mcrService.findByRecieverMSISDN(mobileCallRecordsEntity);	
-	}
-	
-			
-//			Get CellTowerEntity(towerId)
-//			return List<CellTowerEntity>
-	
-	@PostMapping("/cellTowerById")
-	public List<CellTowersEntity> getCellTowersEntityById(@RequestBody CellTowersEntity cellTowerEntity){
-		return ctService.findById(cellTowerEntity);
+	public List<MobileCallRecordsEntity> getCallRecordByReceiverMSISDN(
+			@RequestBody MobileCallRecordsEntity mobileCallRecordsEntity) {
+		return mcrService.findByRecieverMSISDN(mobileCallRecordsEntity);
 	}
 
-	
-	
-//	GET SubscriberRecords(forenames, surname)
-//	return SubscriberRecords : SubscriberRecordsEntity
-	
-	@PostMapping("/subscriberEntityByForenameSurname")
-	public List<SubscriberRecordsEntity> getSubscriberEntitybyForenameSurname(@RequestBody SubscriberRecordsEntity subscriberRecordEntity){
-		return srService.findByForenameSurname(subscriberRecordEntity);
-		
-	}
-	
-	
 //	GetSubscriberRecords(forenames, surname, dateOfBirth, address)
 //	return SubscriberRecordEntity	
-	
+
 	@PostMapping("/subscriberEntityByForenameSurnameDoBAdd")
-	public List<SubscriberRecordsEntity> getSubscriberEntitybyForenameSurnameDoBAdd(@RequestBody SubscriberRecordsEntity subscriberRecordEntity){
+	public List<SubscriberRecordsEntity> getSubscriberEntitybyForenameSurnameDoBAdd(
+			@RequestBody SubscriberRecordsEntity subscriberRecordEntity) {
 		return srService.findByForenameSurnameDoBAdd(subscriberRecordEntity);
-		
+
 	}
-	
-	
-	
 
-//GET SubscriberRecords(Receiver MSISDN)
-//Return Subscriber Record Entity : List <CitizenEntity>	
-
-			
-
-
-//	getCallRecords(phoneNumber)
-//	return List<CallRecordEntity>
-	
 
 }
