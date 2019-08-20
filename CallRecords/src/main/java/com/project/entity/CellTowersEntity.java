f2e6656c8ccf4743760cf2565060465042b682dd
Tania <tdrydendiaz@gmail.com>
2019-08-20 09:34:30 +0100

    first

---
+++ CallRecords/src/main/java/com/project/entity/CellTowersEntity.java
@@ -0,0 +1,77 @@
+package com.project.entity;
+
+public class CellTowersEntity {
+
+	private String cellTowerId;
+	private String operator;
+	private String type;
+	private String latitude;
+	private String longitude;
+	
+	
+public CellTowersEntity() {
+	
+}
+
+
+public CellTowersEntity(String cellTowerId, String operator, String type, String latitude, String longitude) {
+	super();
+	this.cellTowerId = cellTowerId;
+	this.operator = operator;
+	this.type = type;
+	this.latitude = latitude;
+	this.longitude = longitude;
+}
+
+
+public String getCellTowerId() {
+	return cellTowerId;
+}
+
+
+public void setCellTowerId(String cellTowerId) {
+	this.cellTowerId = cellTowerId;
+}
+
+
+public String getOperator() {
+	return operator;
+}
+
+
+public void setOperator(String operator) {
+	this.operator = operator;
+}
+
+
+public String getType() {
+	return type;
+}
+
+
+public void setType(String type) {
+	this.type = type;
+}
+
+
+public String getLatitude() {
+	return latitude;
+}
+
+
+public void setLatitude(String latitude) {
+	this.latitude = latitude;
+}
+
+
+public String getLongitude() {
+	return longitude;
+}
+
+
+public void setLongitude(String longitude) {
+	this.longitude = longitude;
+}
+
+
+}
--- CallRecords/src/main/java/com/project/entity/MobileCallRecordsEntity.java
+++ CallRecords/src/main/java/com/project/entity/MobileCallRecordsEntity.java
@@ -2,4 +2,65 @@ package com.project.entity;
 
 public class MobileCallRecordsEntity {
 
+	private String timestamp;
+	private String callerMSISDN;
+	private String callCellTowerId;
+	private String receiverMSISDN;
+	private String TowerId;
+	
+	public MobileCallRecordsEntity() {
+		
+	}
+
+	public MobileCallRecordsEntity(String timestamp, String callerMSISDN, String callCellTowerId, String receiverMSISDN,
+			String towerId) {
+		super();
+		this.timestamp = timestamp;
+		this.callerMSISDN = callerMSISDN;
+		this.callCellTowerId = callCellTowerId;
+		this.receiverMSISDN = receiverMSISDN;
+		TowerId = towerId;
+	}
+
+	public String getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public String getCallerMSISDN() {
+		return callerMSISDN;
+	}
+
+	public void setCallerMSISDN(String callerMSISDN) {
+		this.callerMSISDN = callerMSISDN;
+	}
+
+	public String getCallCellTowerId() {
+		return callCellTowerId;
+	}
+
+	public void setCallCellTowerId(String callCellTowerId) {
+		this.callCellTowerId = callCellTowerId;
+	}
+
+	public String getReceiverMSISDN() {
+		return receiverMSISDN;
+	}
+
+	public void setReceiverMSISDN(String receiverMSISDN) {
+		this.receiverMSISDN = receiverMSISDN;
+	}
+
+	public String getTowerId() {
+		return TowerId;
+	}
+
+	public void setTowerId(String towerId) {
+		TowerId = towerId;
+	}
+	
+	
 }
