70689615fd21edcdc2582c29db5d4a0defffd61e
Tania <tdrydendiaz@gmail.com>
2019-08-20 15:35:15 +0100

    added repository

--- CallRecords/src/main/java/com/project/repository/SubscriberRecordsRepository.java
+++ CallRecords/src/main/java/com/project/repository/SubscriberRecordsRepository.java
@@ -3,9 +3,11 @@ package com.project.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.project.entity.SubscriberRecordsEntity;
 
+@Repository
 public interface SubscriberRecordsRepository extends MongoRepository<SubscriberRecordsEntity, Long>{
 
 	
