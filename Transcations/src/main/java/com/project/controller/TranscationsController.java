6af40b4d41803b9f4618e2938cb45af0c7fd7bfd
kryan1622 <krystalryan95@hotmail.co.uk>
2019-08-20 12:00:07 +0100

    added bank cards service

--- Transcations/src/main/java/com/project/controller/TranscationsController.java
+++ Transcations/src/main/java/com/project/controller/TranscationsController.java
@@ -6,23 +6,32 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.entities.BankCardEntity;
 import com.project.entities.PeopleBankAccountEntity;
 import com.project.service.BankAccountHoldersService;
+import com.project.service.BankCardService;
 
 @RestController
 @RequestMapping("/transcations")
 public class TranscationsController {
 
 	private BankAccountHoldersService bahService;
+	private BankCardService bcService;
 
 	@Autowired
-	public TranscationsController(BankAccountHoldersService bahService) {
+	public TranscationsController(BankAccountHoldersService bahService, BankCardService bcService) {
 		this.bahService = bahService;
+		this.bcService = bcService;
 	}
 
 	@PostMapping("/getAccountHolder")
 	public PeopleBankAccountEntity getAccountHolder(@RequestBody PeopleBankAccountEntity peopleBankAccountEntity) {
 		return bahService.getBankHolder(peopleBankAccountEntity);
+	}
+
+	@PostMapping("/getBankCard")
+	public BankCardEntity getBankCard(@RequestBody BankCardEntity bankCardEntity) {
+		return bcService.getBankCard(bankCardEntity);
 	}
 
 }
---
+++ Transcations/src/main/java/com/project/service/BankCardService.java
@@ -0,0 +1,9 @@
+package com.project.service;
+
+import com.project.entities.BankCardEntity;
+
+public interface BankCardService {
+
+	BankCardEntity getBankCard(BankCardEntity bankCardEntity);
+
+}
