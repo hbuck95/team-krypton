6d0063e34bd4023fe4b4b90722f571a9955796f7
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-20 14:21:20 +0100

    Initial commit

---
+++ AuditBE/src/main/java/com/project/AuditBeApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditBeApplication.class, args);
+	}
+
+}
