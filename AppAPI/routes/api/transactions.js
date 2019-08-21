77bb6780c25c48e1f39464d43ebdb82beae5cbff
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-21 13:10:21 +0100

    Refactored POST /getAccountAndCardHolder -> POST /getTransactionsForCitizen. Combined /getAllTransactionsForCard into /getTransactionsForCitizen

--- AppAPI/routes/api/transactions.js
+++ AppAPI/routes/api/transactions.js
@@ -14,13 +14,24 @@ const API = "http://localhost:9006/transactions"
 // @desc    Get all a citizens bank account record and bank card information
 // @body    {forename: "", surname: "", homeAddress: ""}
 // @access  Authentication required via JSToken generated via /users/login
-router.post('/getAccountAndCardHolder', auth.required, (req, res) => {
+router.post('/getTransactionsForCitizen', auth.required, (req, res) => {
 
   //The payload object returned when all axios requests have been resolved
   const payload = {
-    accountHolder: null,
-    bankCard: null
+    eposTransactions: null,
+    atmTransactions: null
   };
+
+  const bankCard = {
+    bankCardNumber: null
+  };
+
+  //Body sent with the /getBankCard axios request bellow
+  //accountNumber is required to retrieve bank cards
+  const bankCardBody = {
+    accountNumber: null
+  };
+
 
   // @route  POST http://localhost:9006/transactions/getAccountHolder
   // @desc   Get a citizens bank account record
@@ -32,16 +43,12 @@ router.post('/getAccountAndCardHolder', auth.required, (req, res) => {
       return res.status(400).json({ payload: "Unable to find Account Holder!" });
     }
 
-    //Assign the retrieved bank account record to the payload object
-    payload.accountHolder = response.data;
-
   }).then(() => {
 
     //Body sent with the /getBankCard axios request bellow
     //accountNumber is required to retrieve bank cards
-    let bankCardBody = {
-      accountNumber: payload.accountHolder.accountNumber
-    };
+    bankCardBody.accountNumber = response.data.accountNumber
+    
 
     // @route  POST http://localhost:9006/transactions/getBankCard
     // @desc   Get a citizens bank card information
@@ -54,11 +61,47 @@ router.post('/getAccountAndCardHolder', auth.required, (req, res) => {
       }
 
       //Assign the retrieved bank card information to the payload object
-      payload.bankCard = response.data;
+      //payload.bankCard = response.data;
+
+      bankCard.bankCardNumber = response.data.bankCardNumber;
 
     }).then(() => {
-      //End the request chain by returning the payload object with a status code of OK.
-      return res.status(200).json({ payload: payload });
+
+      // @route  POST http://localhost:9006/transactions/getEposTransactions
+      // @desc   Get all EPOS transactions for a bank card
+      // @body   {bankCardNumber: ""}
+      axios.post(API + "/getEposTransactions", bankCard, { headers: HEADERS }).then(response => {
+
+        //If the status code OK is not returned end the request early and return that the resource cannot be found
+        if (res.statusCode !== 200) {
+          return res.status(400).json({ payload: "Requested resources could not be found." });
+        }
+
+        //Assign the EPOS transaction record(s) to the payload object
+        payload.eposTransactions = response.data;
+
+      })
+
+    }).then(() => {
+
+      // @route  POST http://localhost:9006/transactions/getAtmTransactions
+      // @desc   Get all EPOS transactions for a bank card
+      // @body   {bankCardNumber: ""}
+      axios.post(API + "/getAtmTransactions", bankCard, { headers: HEADERS }).then(response => {
+
+        //If the status code OK is not returned end the request early and return that the resource cannot be found
+        if (res.statusCode !== 200) {
+          return res.status(400).json({ payload: "Requested resources could not be found." });
+        }
+
+        //Assign the retrieved bank card information to the payload object
+        payload.atmTransactions = response.data;
+
+      }).then(() => {
+        //End the request chain by returning the payload object with a status code of OK.
+        return res.status(200).json({ payload: payload });
+      })
+
     })
 
     //Handle any errors thrown up throughout the promise chain
