b8fcc2af2dcab1defd6993ce3d9152736e71c480
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-21 13:29:33 +0100

    Improved documentation

--- AppAPI/routes/api/transactions.js
+++ AppAPI/routes/api/transactions.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const auth = require('../auth');
 const axios = require('axios');
 
+//Headers used in each axios request
 const HEADERS = {
   'Content-Type': 'application/json'
 };
 
+//URL to the transactions microservice
 const API = "http://localhost:9006/transactions"
 
 // @route   POST api/transactions/getAccountAndCardHolder
