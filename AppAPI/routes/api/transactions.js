4550bfa1d7715308a879b251e37440e4813fb193
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-21 11:01:24 +0100

    Added POST /getEposTransactions and /getAtmTransactions requiring {bankCardNumber: xxx}

--- AppAPI/routes/api/transactions.js
+++ AppAPI/routes/api/transactions.js
@@ -49,4 +49,44 @@ router.post('/getBankCard', auth.required, (req, res) => {
 
 });
 
+//POST get citizen data using forenames, surname, and address (authentication required)
+router.post('/getEposTransactions', auth.required, (req, res) => {
+
+  axios.post(API + "/getEposTransactions", req.body, { headers: HEADERS })
+    .then(response => {
+      if (res.statusCode === 200) {
+        console.log(response.data);
+        return res.status(200).json({ payload: response.data });
+      } else {
+        console.log(res.statusCode);
+        console.log(response.data);
+        return res.status(400).json({ payload: "Unable to find EPOS Transaction for the supplied bank card number!" });
+      }
+    }).catch(err => {
+        console.log(err);
+        return res.status(500).json({ error: err});
+    });
+
+});
+
+//POST get citizen data using forenames, surname, and address (authentication required)
+router.post('/getAtmTransactions', auth.required, (req, res) => {
+
+  axios.post(API + "/getAtmTransactions", req.body, { headers: HEADERS })
+    .then(response => {
+      if (res.statusCode === 200) {
+        console.log(response.data);
+        return res.status(200).json({ payload: response.data });
+      } else {
+        console.log(res.statusCode);
+        console.log(response.data);
+        return res.status(400).json({ payload: "Unable to find ATM Transaction for the supplied bank card number!" });
+      }
+    }).catch(err => {
+        console.log(err);
+        return res.status(500).json({ error: err});
+    });
+
+});
+
 module.exports = router;
