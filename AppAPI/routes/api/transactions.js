b874ef1c03e1f5e61d75ca0bd53cca8d66a21bda
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-21 17:18:33 +0100

    Console log statements investigation epos error

--- AppAPI/routes/api/transactions.js
+++ AppAPI/routes/api/transactions.js
@@ -67,24 +67,29 @@ router.post('/getTransactionsForCitizen', auth.required, (req, res) => {
       // @desc   Get all EPOS transactions for a bank card
       // @body   {bankCardNumber: ""}
       axios.post(API + "/getEposTransactions", bankCard, { headers: HEADERS }).then(response => {
+        console.log(bankCard);
 
         //If the status code OK is not returned end the request early and return that the resource cannot be found
         if (res.statusCode !== 200) {
+          console.log(res.statusCode + ": "+ res.statusMessage);
           return res.status(400).json({ payload: "Requested resources could not be found." });
         }
 
         //Assign the EPOS transaction record(s) to the payload object
         payload.eposTransactions = response.data;
 
-      }).catch(err => console.log(err));
+      });
     }).then(() => {
 
       // @desc   Get all EPOS transactions for a bank card
       // @body   {bankCardNumber: ""}
       axios.post(API + "/getAtmTransactions", bankCard, { headers: HEADERS }).then(response => {
 
+        console.log(bankCard);
+
         //If the status code OK is not returned end the request early and return that the resource cannot be found
         if (res.statusCode !== 200) {
+          console.log(res.statusCode + ": "+ res.statusMessage);
           return res.status(400).json({ payload: "Requested resources could not be found." });
         }
 
