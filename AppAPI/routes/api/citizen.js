bed639dde6f7cbce576225f1b5ae6983d72b49c3
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-21 09:29:34 +0100

    Refactored /getCitizen into /citizen route

---
+++ AppAPI/routes/api/citizen.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const auth = require('../auth');
+const axios = require('axios');
+
+const HEADERS = {
+  'Content-Type': 'application/json'
+};
+
+//POST get citizen data using forenames, surname, and address (authentication required)
+router.post('/getCitizen', auth.required, (req, res) => {
+
+    axios.post("http://localhost:9003/citizen/getCitizen", req.body, { headers: HEADERS })
+      .then(response => {
+        if (res.statusCode === 200) {
+          console.log(response.data);
+          return res.status(200).json({ data: response.data });
+        } else {
+          console.log(res.statusCode);
+          console.log(response.data);
+          return res.status(400).json({ data: "Unable to find citizen" });
+        }
+      }).catch(err => {
+          console.log(err);
+          return res.status(500).json({ error: err});
+      });
+  
+  });
+
+  module.exports = router;
--- AppAPI/routes/api/users.js
+++ AppAPI/routes/api/users.js
@@ -3,11 +3,6 @@ const passport = require('passport');
 const router = require('express').Router();
 const auth = require('../auth');
 const Users = mongoose.model('Users');
-const axios = require('axios');
-
-const HEADERS = {
-  'Content-Type': 'application/json'
-};
 
 //POST new user route (optional, everyone has access)
 router.post('/', auth.optional, (req, res, next) => {
@@ -85,26 +80,5 @@ router.get('/current', auth.required, (req, res, next) => {
       return res.json({ user: user.toAuthJSON() });
     });
 });
-
-//POST get citizen data using forenames, surname, and address (authentication required)
-router.post('/getCitizen', auth.required, (req, res) => {
-
-  axios.post("http://localhost:9003/citizen/getCitizen", req.body, { headers: HEADERS })
-    .then(response => {
-      if (res.statusCode === 200) {
-        console.log(response.data);
-        return res.status(200).json({ data: response.data });
-      } else {
-        console.log(res.statusCode);
-        console.log(response.data);
-        return res.status(400).json({ data: "Unable to find citizen" });
-      }
-    }).catch(err => {
-        console.log(err);
-        return res.status(500).json({ error: err});
-    });
-
-});
-
 
 module.exports = router;
