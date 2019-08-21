a6db2ffecdc8b6d2b59a90a42acdc1cca897b7dc
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-21 11:31:38 +0100

    Created POST /getAccountAndCardHolder chaining /getAccountHolder and /getBankCard together

--- AppAPI/routes/api/transactions.js
+++ AppAPI/routes/api/transactions.js
@@ -9,6 +9,39 @@ const HEADERS = {
 const API = "http://localhost:9006/transactions"
 
 
+router.post('/getAccountAndCardHolder', auth.required, (req,res) => {
+  axios.post(API + "/getAccountHolder", req.body, {headers: HEADERS }).then(response => {
+    if(res.statusCode === 200){
+      return res.status(200).json(({payload : response.data}));
+    } else {
+      return res.status(400).json({ payload: "Unable to find Account Holder!"});
+    }
+
+    //accountNumber
+  }).then(() => {
+
+    let bankCardBody = {
+      accountNumber: req.body.accountNumber
+    };
+
+    axios.post(API + "/getBankCard", bankCardBody, { headers: HEADERS })
+    .then(response => {
+      if (res.statusCode === 200) {
+        console.log(response.data);
+        return res.status(200).json({ payload: response.data });
+      } else {
+        console.log(res.statusCode);
+        console.log(response.data);
+        return res.status(400).json({ payload: "Unable to find Bank Card!" });
+      }
+    })
+  }).catch(err => {
+    console.log(err);
+    return res.status(500).json({error: err});
+  });
+
+});
+
 //POST get citizen data using forenames, surname, and address (authentication required)
 router.post('/getAccountHolder', auth.required, (req, res) => {
 
