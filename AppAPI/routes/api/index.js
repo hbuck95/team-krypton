7e72937bb410ef93c13dfe305b6d3a5ef3dce4b6
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-21 10:41:03 +0100

    Required the /transactions route

--- AppAPI/routes/api/index.js
+++ AppAPI/routes/api/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.use('/users', require('./users'));
 router.use('/citizen', require('./citizen'));
+router.use('/transactions', require('./transactions'));
 
 module.exports = router;
