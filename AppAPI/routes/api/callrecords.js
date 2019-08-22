834d4187d8cfa0fe4d167ab606a1f959dfcdfdf0
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-22 16:04:45 +0100

    Created POST /getCellTowers

--- AppAPI/routes/api/callrecords.js
+++ AppAPI/routes/api/callrecords.js
@@ -10,16 +10,31 @@ const HEADERS = {
 //URL to the transactions microservice
 const API = "http://localhost:9004/call"
 
+
+// @route  POST http://localhost:9004/call/getPhoneNumber
+// @desc   Get all EPOS transactions for a bank card
+// @body   {forename: "", surname: "", homeAddress: ""}
+// @return  axios.post(API + "/getPhoneNumber", req.body, { headers: HEADERS })
 const getPhoneNumber = (body) => {
     return axios.post(API + "/getPhoneNumber", body, { headers: HEADERS });
 };
 
+// @route  POST http://localhost:9004/call/getCallRecordsOfSuspect
+// @desc   Get all mobile call records where the retrieved phone number is the caller
 const getCallRecordsOfSuspect = (body) => {
     return axios.post(API + "/getCallRecordsOfSuspect", body, { headers: HEADERS })
 };
 
+// @route  POST http://localhost:9004/call/getCallRecordsOfSuspect
+// @desc   Get all people mobile phone records where the supplied phone number is the receiver of the above calls
 const getAssociates = (body) => {
     return axios.post(API + "/getAssosiate", body, { headers: HEADERS })
+};
+
+// @route  POST http://localhost:9004/call/getCellTower
+// @desc   Get the cell towers which routed the above calls.
+const getCellTower = (body) => {
+    return axios.post(API + "/getCellTower", body, { headers: HEADERS })
 };
 
 
@@ -35,35 +50,50 @@ router.post('/getAssociates', auth.required, (req, res) => {
         callerMSISDN: null
     };
 
-    // @route  POST http://localhost:9004/call/getPhoneNumber
-    // @desc   Get all EPOS transactions for a bank card
-    // @body   {forename: "", surname: "", homeAddress: ""}
-    // return axios.post(API + "/getPhoneNumber", req.body, { headers: HEADERS })
     getPhoneNumber(req.body)
         .then(response => {
-
             //Assign the phone number retrieved to the suspectcallrecordsbody object for the next axios request.
             suspectCallRecordsBody.callerMSISDN = response.data.phoneNumber;
-
-            // @route  POST http://localhost:9004/call/getCallRecordsOfSuspect
-            // @desc   Get all mobile call records where the retrieved phone number is the caller
-            //return axios.post(API + "/getCallRecordsOfSuspect", suspectCallRecordsBody, { headers: HEADERS })
             return getCallRecordsOfSuspect(suspectCallRecordsBody)
         }).then(response => {
-
-            // @route  POST http://localhost:9004/call/getCallRecordsOfSuspect
-            // @desc   Get all people mobile phone records where the supplied phone number is the receiver of the above calls
-            //return axios.post(API + "/getAssosiate", response.data, { headers: HEADERS })
             return getAssociates(response.data)
         }).then(response => {
-
             payload.associates = response.data;
             return res.status(200).json(payload).end();
-
         }).catch(err => {
             console.log(err);
             return res.status(500).json({ message: "An error ocurred whilst processing your request.", error: err });
         })
+});
+
+router.post("/getCellTowers", auth.required, (req, res) => {
+
+    const payload = {
+        cellTowers: null
+    };
+
+    const suspectCallRecordsBody = {
+        callerMSISDN: null
+    };
+
+    getPhoneNumber(req.body)
+        .then(response => {
+            //Assign the phone number retrieved to the suspectcallrecordsbody object for the next axios request.
+            suspectCallRecordsBody.callerMSISDN = response.data.phoneNumber;
+            return getCallRecordsOfSuspect(suspectCallRecordsBody)
+        }).then(response => {
+            return getCellTower(response.data)
+        })
+        .then(response => {
+            payload.cellTowers = response.data;
+        })
+        .then(() => {
+            res.status(200).json(payload).end();
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(500).json({ message: "An error ocurred whilst processing your request.", error: err }).end();
+        });
 
 });
 
