0be0680de07960d673723fa9d0aa7941838932fb
kryan1622 <krystalryan95@hotmail.co.uk>
2019-08-22 12:42:13 +0100

    added getters and setters

--- Transcations/src/main/java/com/project/entities/AtmtransactionsEntity.java
+++ Transcations/src/main/java/com/project/entities/AtmtransactionsEntity.java
@@ -31,6 +31,38 @@ public class AtmtransactionsEntity {
 		this.amount = amount;
 	}
 
+	public String getTimeStamp() {
+		return timeStamp;
+	}
+
+	public void setTimeStamp(String timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public void setAtmId(int atmId) {
+		this.atmId = atmId;
+	}
+
+	public void setBankCardNumber(Long bankCardNumber) {
+		this.bankCardNumber = bankCardNumber;
+	}
+
 	public Long getBankCardNumber() {
 		return bankCardNumber;
 	}
--- Transcations/src/main/java/com/project/entities/PeopleBankAccountEntity.java
+++ Transcations/src/main/java/com/project/entities/PeopleBankAccountEntity.java
@@ -29,6 +29,50 @@ public class PeopleBankAccountEntity {
 		this.homeAddress = homeAddress;
 	}
 
+	public int getBankAccountId() {
+		return bankAccountId;
+	}
+
+	public void setBankAccountId(int bankAccountId) {
+		this.bankAccountId = bankAccountId;
+	}
+
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public String getBank() {
+		return bank;
+	}
+
+	public void setBank(String bank) {
+		this.bank = bank;
+	}
+
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public void setForenames(String forenames) {
+		this.forenames = forenames;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public void setHomeAddress(String homeAddress) {
+		this.homeAddress = homeAddress;
+	}
+
 	public String getForenames() {
 		return forenames;
 	}
