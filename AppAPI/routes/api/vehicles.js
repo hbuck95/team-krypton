ced2eaff838b5bedff7fb0211141b8e313cd8691
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-22 18:01:53 +0100

    Created POST /getVehicle route

--- AppAPI/routes/api/vehicles.js
+++ AppAPI/routes/api/vehicles.js
@@ -3,8 +3,34 @@ const auth = require('../auth');
 const axios = require('axios');
 
 const HEADERS = {
-  'Content-Type': 'application/json'
+    'Content-Type': 'application/json'
 };
+
+//URL to the transactions microservice
+const API = "http://localhost:9005/ANPR"
+
+const findVehicleRegistration = (body) => {
+    return axios.post(API + "/getVehicle", body, { headers: HEADERS });
+};
+
+axios.post("/getVehicle", auth.required, (req, res) => {
+
+    const payload = {
+        vehicleRegistration: null
+    };
+
+    findVehicleRegistration(req.body)
+        .then(response => {
+            payload.vehicleRegistration = response.data;
+        })
+        .then(() => {
+            res.status(200).json(payload).end();
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(500).json({ message: "An error ocurred whilst processing your request.", error: err });
+        });
+});
 
 
 module.exports = router;
