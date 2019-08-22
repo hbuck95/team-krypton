6c09181b826ca6a2db1fe2ebaff0b3d664b382a2
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-22 11:05:03 +0100

    Defined query for findByForenames

--- CallRecords/src/main/java/com/project/repository/PeopleMobileRepository.java
+++ CallRecords/src/main/java/com/project/repository/PeopleMobileRepository.java
@@ -1,6 +1,7 @@
 package com.project.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.project.entities.PeopleMobileEntity;
 
@@ -8,6 +9,7 @@ public interface PeopleMobileRepository extends MongoRepository<PeopleMobileEnti
 
 	PeopleMobileEntity findByPhoneNumber(String phoneNumber);
 
+	@Query("{ 'forenames' : ?0, 'surname' : ?1, 'address' : ?2}")
 	PeopleMobileEntity findByForenames(String forenames, String surname, String address);
 
 }
