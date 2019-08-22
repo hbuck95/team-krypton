17d144f203f157ddebce390a2c40346bf05587d7
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-22 10:20:02 +0100

    Created a callreords route (/callrecords)

--- AppAPI/routes/api/index.js
+++ AppAPI/routes/api/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 router.use('/users', require('./users'));
 router.use('/citizen', require('./citizen'));
 router.use('/transactions', require('./transactions'));
+router.use('/callrecords', require('./callrecords'));
 
 module.exports = router;
