df05decb4a115408fcb270986cb4897eb836f309
kryan1622 <krystalryan95@hotmail.co.uk>
2019-08-22 12:44:49 +0100

    saved

--- Transcations/src/main/java/com/project/entities/BankCardEntity.java
+++ Transcations/src/main/java/com/project/entities/BankCardEntity.java
@@ -31,4 +31,48 @@ public class BankCardEntity {
 		return accountNumber;
 	}
 
+	public int getBankcardId() {
+		return bankcardId;
+	}
+
+	public void setBankcardId(int bankcardId) {
+		this.bankcardId = bankcardId;
+	}
+
+	public Long getCardNumber() {
+		return cardNumber;
+	}
+
+	public void setCardNumber(Long cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+
+	public String getSortCode() {
+		return sortCode;
+	}
+
+	public void setSortCode(String sortCode) {
+		this.sortCode = sortCode;
+	}
+
+	public int getBankAccountId() {
+		return bankAccountId;
+	}
+
+	public void setBankAccountId(int bankAccountId) {
+		this.bankAccountId = bankAccountId;
+	}
+
+	public String getBank() {
+		return bank;
+	}
+
+	public void setBank(String bank) {
+		this.bank = bank;
+	}
+
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
 }
