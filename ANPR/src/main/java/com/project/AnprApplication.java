29be995ac6701eacc076d6a0946df2c1fef8098a
kryan1622 <krystalryan95@hotmail.co.uk>
2019-08-22 18:21:19 +0100

    added beans

--- ANPR/src/main/java/com/project/AnprApplication.java
+++ ANPR/src/main/java/com/project/AnprApplication.java
@@ -2,6 +2,9 @@ package com.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class AnprApplication {
@@ -10,4 +13,8 @@ public class AnprApplication {
 		SpringApplication.run(AnprApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
 }
