d8cdbc52ee1c30e5155286c24e4f6db1f1d996d4
kryan1622 <krystalryan95@hotmail.co.uk>
2019-08-22 18:28:15 +0100

    added query

--- ANPR/src/main/java/com/project/repository/VehicleRepo.java
+++ ANPR/src/main/java/com/project/repository/VehicleRepo.java
@@ -1,6 +1,7 @@
 package com.project.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.project.entity.VehicleEntity;
@@ -8,5 +9,6 @@ import com.project.entity.VehicleEntity;
 @Repository
 public interface VehicleRepo extends MongoRepository<VehicleEntity, Long> {
 
+	@Query("{ 'vehicleRegistrationNo' : ?0}")
 	VehicleEntity findbyVehicleRegistrationNo(String vehicleRegistrationNo);
 }
