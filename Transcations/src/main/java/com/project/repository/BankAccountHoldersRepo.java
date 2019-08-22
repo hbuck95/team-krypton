f1c6042bf2b9610509f2b5ca7d5857a0dc6f27a2
kryan1622 <krystalryan95@hotmail.co.uk>
2019-08-22 12:23:16 +0100

    added Query annotation

--- Transcations/src/main/java/com/project/repository/BankAccountHoldersRepo.java
+++ Transcations/src/main/java/com/project/repository/BankAccountHoldersRepo.java
@@ -1,6 +1,7 @@
 package com.project.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.project.entities.PeopleBankAccountEntity;
@@ -8,5 +9,6 @@ import com.project.entities.PeopleBankAccountEntity;
 @Repository
 public interface BankAccountHoldersRepo extends MongoRepository<PeopleBankAccountEntity, Long> {
 
+	@Query("{ 'forenames' : ?0, 'surname' : ?1, 'address' : ?2}")
 	PeopleBankAccountEntity findByForenames(String forenames, String surname, String homeAddress);
 }
