47d804de4ef0325e052dad6fa79fed048acd32ed
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-22 09:50:31 +0100

    Attempting to fix issue regarding chaining axios promises

--- AppAPI/routes/api/transactions.js
+++ AppAPI/routes/api/transactions.js
@@ -111,4 +111,93 @@ router.post('/getTransactionsForCitizen', auth.required, (req, res) => {
 });
 
 
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+
+router.post('/test', auth.required, (req, res) => {
+
+
+  //The payload object returned when all axios requests have been resolved
+  const payload = {
+    eposTransactions: null,
+    atmTransactions: null
+  };
+
+  //Body sent with the /getBankCard axios request bellow
+  //accountNumber is required to retrieve bank cards
+  const bankCardBody = {
+    accountNumber: null
+  };
+
+  //The bankCard object used in the body of the /getEposTransactions and /getAtmTransactions axios request.
+  const bankCard = {
+    bankCardNumber: null
+  };
+
+
+
+  return axios.post(API + "/getAccountHolder", req.body, { headers: HEADERS })
+    .then(response => {
+
+      //If the status code OK is not returned end the request early and return that the account holder cannot be found
+      if (res.statusCode !== 200) {
+        return res.status(400).json({ payload: "Unable to find Account Holder!" });
+      }
+
+      // Assign the accountNumber from the retrieved record to the bankCardBody object.
+      bankCardBody.accountNumber = response.data.accountNumber
+      return axios.post(API + "/getBankCard", bankCardBody, { headers: HEADERS })
+
+    }).then(response => {
+      if (res.statusCode !== 200) {
+        return res.status(400).json({ payload: "Unable to find Bank Card!" });
+      }
+
+      //Assign the retrieved bank card information to the bankCard object
+      bankCard.bankCardNumber = response.data.cardNumber;
+      return axios.post(API + "/getEposTransactions", bankCard, { headers: HEADERS })
+
+    }).then(response => {
+      console.log(bankCard);
+
+      //If the status code OK is not returned end the request early and return that the resource cannot be found
+      if (res.statusCode !== 200) {
+        console.log(res.statusCode + ": " + res.statusMessage);
+        return res.status(400).json({ payload: "Requested resources could not be found." });
+      }
+
+      //Assign the EPOS transaction record(s) to the payload object
+      payload.eposTransactions = response.data;
+
+      return axios.post(API + "/getAtmTransactions", bankCard, { headers: HEADERS })
+
+    }).then(response => {
+
+      console.log(bankCard);
+
+      //If the status code OK is not returned end the request early and return that the resource cannot be found
+      if (res.statusCode !== 200) {
+        console.log(res.statusCode + ": " + res.statusMessage);
+        return res.status(400).json({ payload: "Requested resources could not be found." });
+      }
+
+      //Assign the retrieved bank card information to the payload object
+      payload.atmTransactions = response.data;
+
+    }).then(() => {
+      return res.status(200).json({ payload: payload });
+    }).catch(err => {
+      console.log(err);
+      return res.status(500).json({ error: err });
+
+    })
+});
+
+
+
+
+
+
+
+
 module.exports = router;
