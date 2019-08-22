4a51470be265708084f5d83fa7921dc73d069142
kryan1622 <krystalryan95@hotmail.co.uk>
2019-08-22 18:17:09 +0100

    added annotation

--- ANPR/src/main/java/com/project/repository/VehicleRepo.java
+++ ANPR/src/main/java/com/project/repository/VehicleRepo.java
@@ -1,9 +1,11 @@
 package com.project.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.project.entity.VehicleEntity;
 
+@Repository
 public interface VehicleRepo extends MongoRepository<VehicleEntity, Long> {
 
 	VehicleEntity findVehicleRegistrationNo(String vehicleRegistrationNo);
