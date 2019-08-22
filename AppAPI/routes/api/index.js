79a8a7ef873cddf388bc97c2779bc32928a05623
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-22 17:52:57 +0100

    Created /vehicles route

--- AppAPI/routes/api/index.js
+++ AppAPI/routes/api/index.js
@@ -5,5 +5,6 @@ router.use('/users', require('./users'));
 router.use('/citizen', require('./citizen'));
 router.use('/transactions', require('./transactions'));
 router.use('/callrecords', require('./callrecords'));
+router.use('/vehicle', require('./vehicles'));
 
 module.exports = router;
---
+++ AppAPI/routes/api/vehicles.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const auth = require('../auth');
+const axios = require('axios');
+
+const HEADERS = {
+  'Content-Type': 'application/json'
+};
+
+
+module.exports = router;
