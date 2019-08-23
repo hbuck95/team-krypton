38b172994d6ace4f5cda6e32dfe3bfdcf57221d7
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-23 15:41:41 +0100

    Added documentation for /getCitizen

--- AppAPI/routes/api/citizen.js
+++ AppAPI/routes/api/citizen.js
@@ -3,6 +3,10 @@ const auth = require('../auth');
 const makeRequest = require('../../util/makeRequest');
 
 const API = "http://localhost:9003/citizen"
+
+// @route  POST http://localhost:9003/citizen/getCitizen
+// @desc   Get a citizen record based on their forenames, surname, and address
+// @body   {"forenames":"Charlene Danielle", "surname":"Francis", "homeAddress":"34 KING CROSS ROAD, HALIFAX, HX1 3LN"}
 const GET_CITIZEN = "/getCitizen";
 
 router.post('/getCitizen', auth.required, (req, res) => {
@@ -12,7 +16,7 @@ router.post('/getCitizen', auth.required, (req, res) => {
         })
         .catch(err => {
             console.log(err);
-            return res.status(500).json({ message: "An error ocurred whilst processing your request.", error: err });            
+            return res.status(500).json({ message: "An error ocurred whilst processing your request.", error: err });
         });
 });
 
