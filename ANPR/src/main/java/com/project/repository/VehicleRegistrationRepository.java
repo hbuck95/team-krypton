74a115477b419e3bd82915df97edb18de94cd1a3
kryan1622 <krystalryan95@hotmail.co.uk>
2019-08-23 09:10:51 +0100

    added getters and setters back

--- ANPR/src/main/java/com/project/repository/VehicleRegistrationRepository.java
+++ ANPR/src/main/java/com/project/repository/VehicleRegistrationRepository.java
@@ -3,6 +3,7 @@ package com.project.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.project.entity.VehicleRegistrationEntity;
@@ -10,6 +11,7 @@ import com.project.entity.VehicleRegistrationEntity;
 @Repository
 public interface VehicleRegistrationRepository extends MongoRepository<VehicleRegistrationEntity, Long> {
 
+	@Query("{ 'forenames' : ?0, 'surname' : ?1 }")
 	List<VehicleRegistrationEntity> findByForenamesAndSurname(String forenames, String surname);
 
 }
