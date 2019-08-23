8d005950bc01d4b773bd6ce6d809a1ca30712403
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-23 13:08:21 +0100

    Removed obsolete code from callrecords.js

--- AppAPI/routes/api/callrecords.js
+++ AppAPI/routes/api/callrecords.js
@@ -12,39 +12,23 @@ const HEADERS = {
 //URL to the transactions microservice
 const API = "http://localhost:9004/call"
 
-const GET_PHONE_NUMBER = "/getPhoneNumber";
-const GET_CALL_RECORDS_OF_SUSPECT = "/getCallRecordsOfSuspect";
-const GET_ASSOCIATES = "/getAssosiate";
-const GET_CELL_TOWER = "/getCellTower";
-
-
 // @route  POST http://localhost:9004/call/getPhoneNumber
 // @desc   Get all EPOS transactions for a bank card
 // @body   {forename: "", surname: "", homeAddress: ""}
 // @return  axios.post(API + "/getPhoneNumber", req.body, { headers: HEADERS })
-const getPhoneNumber = (body) => {
-    return axios.post(API + "/getPhoneNumber", body, { headers: HEADERS });
-};
+const GET_PHONE_NUMBER = "/getPhoneNumber";
 
 // @route  POST http://localhost:9004/call/getCallRecordsOfSuspect
 // @desc   Get all mobile call records where the retrieved phone number is the caller
-const getCallRecordsOfSuspect = (body) => {
-    return axios.post(API + "/getCallRecordsOfSuspect", body, { headers: HEADERS })
-};
+const GET_CALL_RECORDS_OF_SUSPECT = "/getCallRecordsOfSuspect";
 
 // @route  POST http://localhost:9004/call/getCallRecordsOfSuspect
 // @desc   Get all people mobile phone records where the supplied phone number is the receiver of the above calls
-const getAssociates = (body) => {
-    return axios.post(API + "/getAssosiate", body, { headers: HEADERS })
-};
+const GET_ASSOCIATES = "/getAssosiate";
 
 // @route  POST http://localhost:9004/call/getCellTower
 // @desc   Get the cell towers which routed the above calls.
-const getCellTower = (body) => {
-    return axios.post(API + "/getCellTower", body, { headers: HEADERS })
-};
-
-
+const GET_CELL_TOWER = "/getCellTower";
 
 router.post('/getAssociates', auth.required, (req, res) => {
 
