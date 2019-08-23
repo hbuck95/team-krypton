bda34a2dfe79ca082dfe7f651431b8ac70a7c446
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-23 15:37:10 +0100

    Removed obsolete code from callrecords

--- AppAPI/routes/api/callrecords.js
+++ AppAPI/routes/api/callrecords.js
@@ -1,13 +1,7 @@
 const router = require('express').Router();
 const auth = require('../auth');
-const axios = require('axios');
 const _ = require('lodash');
 const makeRequest = require('../../util/makeRequest');
-
-//Headers used in each axios request
-const HEADERS = {
-    'Content-Type': 'application/json'
-};
 
 //URL to the transactions microservice
 const API = "http://localhost:9004/call"
