5d861499b57184690ebf811d0e7f1618987c7c7a
kryan1622 <krystalryan95@hotmail.co.uk>
2019-08-23 12:00:58 +0100

    added beans

--- CallRecords/src/main/java/com/project/repository/PeopleMobileRepository.java
+++ CallRecords/src/main/java/com/project/repository/PeopleMobileRepository.java
@@ -2,9 +2,11 @@ package com.project.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.project.entities.PeopleMobileEntity;
 
+@Repository
 public interface PeopleMobileRepository extends MongoRepository<PeopleMobileEntity, Long> {
 
 	PeopleMobileEntity findByPhoneNumber(String phoneNumber);
