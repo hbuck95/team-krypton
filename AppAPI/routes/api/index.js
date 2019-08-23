b449752f16b4b2e265008f4950eaff59e7747208
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-23 16:12:18 +0100

    Added documentation

--- AppAPI/routes/api/index.js
+++ AppAPI/routes/api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+//Routes
 router.use('/users', require('./users'));
 router.use('/citizen', require('./citizen'));
 router.use('/transactions', require('./transactions'));
