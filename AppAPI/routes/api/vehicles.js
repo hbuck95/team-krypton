d26de92e03e1d8c5d6bc7a020efd982d57204abc
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-23 09:58:53 +0100

    Added POST /getVehicleRegistrations

--- AppAPI/routes/api/vehicles.js
+++ AppAPI/routes/api/vehicles.js
@@ -1,39 +1,43 @@
 const router = require('express').Router();
 const auth = require('../auth');
-const axios = require('axios');
-
 const makeRequest = require('../../util/makeRequest');
-
-const HEADERS = {
-    'Content-Type': 'application/json'
-};
 
 //URL to the transactions microservice
 const API = "http://localhost:9005/ANPR"
 
+//Endpoint URLs
 const GET_VEHICLE_REGISTRATIONS = "/getVehicleRegistrations";
 const GET_VEHICLE = "/getVehicle";
-
-const findVehicleRegistration = (body) => {
-    return axios.post(API + "/getVehicleRegistrations", body, { headers: HEADERS });
-};
-
-const makeAxiosPost = (endpoint, body) => {
-    return axios.post(API + endpoint, body, {headers: HEADERS});
-};
 
 router.post("/getVehicle", auth.required, (req, res) => {
 
     const payload = {
-        vehicleRegistration: null
+        vehicle: null
     };
 
-    //makeAxiosPost(GET_VEHICLE_REGISTRATIONS, req.body)
     makeRequest.axiosPost(API + GET_VEHICLE, req.body)
         .then(response => {
-            payload.vehicleRegistration = response.data;
+            payload.vehicle = response.data;
         })
         .then(() => {
+            res.status(200).json(payload).end();
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(500).json({ message: "An error ocurred whilst processing your request.", error: err });
+        });
+});
+
+router.post("/getVehicleRegistrations", auth.required, (req, res) => {
+
+    const payload = {
+        vehicleRegistrations = null
+    };
+
+    makeRequest.axiosPost(API + GET_VEHICLE_REGISTRATIONS, req.body)
+        .then(response => {
+            payload.vehicleRegistrations = response.data;
+        }).then(() => {
             res.status(200).json(payload).end();
         })
         .catch(err => {
