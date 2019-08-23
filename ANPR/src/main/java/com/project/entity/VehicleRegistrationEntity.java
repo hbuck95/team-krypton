322ce4d5a6238bc0d36edeea7175a765b14901fa
kryan1622 <krystalryan95@hotmail.co.uk>
2019-08-23 09:10:51 +0100

    added getters and setters back

--- ANPR/src/main/java/com/project/entity/VehicleRegistrationEntity.java
+++ ANPR/src/main/java/com/project/entity/VehicleRegistrationEntity.java
@@ -3,7 +3,7 @@ package com.project.entity;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-@Document(collection = "vehicleRegistration")
+@Document(collection = "vehicleregistration")
 public class VehicleRegistrationEntity {
 
 	@Field("registrationID")
@@ -38,6 +38,86 @@ public class VehicleRegistrationEntity {
 
 	public VehicleRegistrationEntity() {
 
+	}
+
+	public String getRegistrationId() {
+		return registrationId;
+	}
+
+	public void setRegistrationId(String registrationId) {
+		this.registrationId = registrationId;
+	}
+
+	public String getRegistrationDate() {
+		return registrationDate;
+	}
+
+	public void setRegistrationDate(String registrationDate) {
+		this.registrationDate = registrationDate;
+	}
+
+	public String getVehicleRegistrationNo() {
+		return vehicleRegistrationNo;
+	}
+
+	public void setVehicleRegistrationNo(String vehicleRegistrationNo) {
+		this.vehicleRegistrationNo = vehicleRegistrationNo;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getDriverLicenceID() {
+		return driverLicenceID;
+	}
+
+	public void setDriverLicenceID(String driverLicenceID) {
+		this.driverLicenceID = driverLicenceID;
+	}
+
+	public void setForenames(String forenames) {
+		this.forenames = forenames;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
 	}
 
 	public String getForenames() {
