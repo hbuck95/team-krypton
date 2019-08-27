2c8e6360d733be8907ffe2ad4f601385f33b14ab
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-27 14:09:24 +0100

    Removed branch files accidentally added during merge pr

--- ANPR/src/main/java/com/project/service/VehicleService.java
+++
@@ -1,9 +0,0 @@
-package com.project.service;
-
-import com.project.entity.VehicleEntity;
-
-public interface VehicleService {
-
-	public VehicleEntity findVehicleByRegistration(VehicleEntity vehicleEntity);
-
-}
