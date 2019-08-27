7ec67696e6e37fb9ead8bb16412ab84f02507f5a
Tania <tdrydendiaz@gmail.com>
2019-08-27 14:17:59 +0100

    2/2 service tests

--- ANPR/src/test/java/com/project/service/ANPRObservationServiceTest.java
+++ ANPR/src/test/java/com/project/service/ANPRObservationServiceTest.java
@@ -14,6 +14,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.project.entity.ANPRObservationEntity;
+import com.project.entity.VehicleRegistrationEntity;
 import com.project.repository.ANPRObservationRepository;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -30,20 +31,31 @@ public class ANPRObservationServiceTest {
 			"2015-05-01T09:08:51.975", "IU22 HFF");
 	private static final ANPRObservationEntity ANPR_OBSERVATION2 = new ANPRObservationEntity(5538,
 			"2015-05-01T09:10:45.975", "IU22 HFF");
+	private static final VehicleRegistrationEntity VEHICLE1 = new VehicleRegistrationEntity("131249", "11/04/1995",
+			"IU22 HFF", "BMW", "1 Series", "white", "Patricia Jane", "Adams", "3 CANNON LANE, PINNER, HA5 1HH",
+			"07/04/1989", "ADAMS854079PJ9MU 50");
+//	@Test
+//	public void findByVehicleRegistrationNumber() {
+//		List<ANPRObservationEntity> MOCK_LIST = new ArrayList<>();
+//		MOCK_LIST.add(ANPR_OBSERVATION1);
+//		MOCK_LIST.add(ANPR_OBSERVATION2);
+//		Mockito.when(repo.findByVehicleRegistrationNumber("IU22 HFF")).thenReturn(MOCK_LIST);
+//		assertEquals(MOCK_LIST, service.findByVehicleRegistrationNumber(ANPR_OBSERVATION1));
+//		Mockito.verify(repo).findByVehicleRegistrationNumber("IU22 HFF");
+
+	
 
 	@Test
 	public void findByVehicleRegistrationNumber() {
-		List<ANPRObservationEntity> MOCK_LIST = new ArrayList<>();
-		MOCK_LIST.add(ANPR_OBSERVATION1);
-		MOCK_LIST.add(ANPR_OBSERVATION2);
-		Mockito.when(repo.findByVehicleRegistrationNumber("IU22 HFF")).thenReturn(MOCK_LIST);
-		assertEquals(MOCK_LIST, service.findByVehicleRegistrationNumber(ANPR_OBSERVATION1));
-		Mockito.verify(repo).findByVehicleRegistrationNumber("IU22 HFF");
+List<ANPRObservationEntity> Mock_List = new ArrayList<>();
+Mock_List.add(ANPR_OBSERVATION1);
+List<VehicleRegistrationEntity> MOCK_LIST = new ArrayList<>();
+MOCK_LIST.add(VEHICLE1);
+Mockito.when(repo.findByVehicleRegistrationNumber("IU22 HFF")).thenReturn(Mock_List);
+assertEquals(Mock_List, service.findByVehicleRegistrationNumber(MOCK_LIST));
+Mockito.verify(repo).findByVehicleRegistrationNumber("IU22 HFF");
 
 	}
-	
-	
-	
 
 	@Test
 	public void defaultConstructorTest() {
